c776e032488f46ab24489addb690afe9c69d41c9
rakshith299 <133012343+rakshith299@users.noreply.github.com>
2023-06-14 11:23:07 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,51 @@
 function threeSum(arr, target) {
-// write your code here
+
+	for(let i = 0; i < arr.length; i++){
+        for(let j = i+1; j < arr.length; j++){
+            if(arr[i] > arr[j]){
+                let temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+            }
+        }
+    }
+
+    let stop = arr.length - 3;
+    let closeSum = 0;
+    let mindiff = 999999;
+
+    for(let i = 0; i <= stop; i++){
+        let fixed = arr[i];
+        let start = i+1;
+        let end = arr.length - 1;
+
+        while(start < end){
+            let sum = fixed + arr[start] + arr[end];
+            console.log(`sum ` + sum);
+
+            if(sum == target){
+                closeSum =  sum;
+                break;
+            }else if(sum < target){
+                start = start + 1;
+            }else if(sum > target){
+                end = end - 1;
+            }
+
+            let diff = Math.abs(sum - target);
+
+            if(diff < mindiff){
+                mindiff = diff;
+                closeSum = sum;
+            }
+
+        }
+
+
+    }
+
+
+    return closeSum;
   
 }
 
